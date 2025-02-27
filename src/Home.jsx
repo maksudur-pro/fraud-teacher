80b914386f5d8e4a3669d971c4dadc69851b7dcc
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-02-27 11:29:10 +0600

    added bg section

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <>
+      <div
+        className="w-full h-[550px] md:h-[650px] lg:h-[790px] bg-cover bg-center"
+        style={{ backgroundImage: "url('../src/assets/intro_bg.png')" }}></div>
+    </>
+  );
+};
+
+export default Home;
