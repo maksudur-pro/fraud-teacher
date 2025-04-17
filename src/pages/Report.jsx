f08846845e906c4825e5f8b3b166462d36ec6fab
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-04-17 13:40:19 +0600

    added report pge

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,35 +1,72 @@
 const Report = () => {
   return (
-    <div className="grid grid-cols-2 gap-10 p-8">
-      <input
-        type="text"
-        placeholder="neutral"
-        className="input input-neutral"
-      />
-      <input
-        type="text"
-        placeholder="Primary"
-        className="input input-primary"
-      />
-      <input
-        type="text"
-        placeholder="Secondary"
-        className="input input-secondary"
-      />
-      <input type="text" placeholder="Accent" className="input input-accent" />
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl">
+        <div className="max-w-md mx-auto space-y-6">
+          <div className="h-8">
+            <h1 className="text-2xl uppercase text-center font-bold">Report</h1>
+          </div>
 
-      <input type="text" placeholder="Info" className="input input-info" />
-      <input
-        type="text"
-        placeholder="Success"
-        className="input input-success"
-      />
-      <input
-        type="text"
-        placeholder="Warning"
-        className="input input-warning"
-      />
-      <input type="text" placeholder="Error" className="input input-error" />
+          <form>
+            <legend className="mb-2 text-sm font-medium">Report type *</legend>
+            <select className="select select-neutral rounded-sm border border-black ">
+              <option disabled={true}>Pick a type</option>
+              <option>Tutor</option>
+              <option>Guardian</option>
+              <option>Tuition media</option>
+            </select>
+
+            <legend className="my-2 text-sm font-medium">Your name *</legend>
+            <input
+              type="text"
+              className="input rounded-sm border border-black w-full"
+              placeholder="Enter your name"
+            />
+
+            <legend className="my-2 text-sm font-medium">Phone number *</legend>
+            <input
+              type="number"
+              className="input rounded-sm border border-black w-full"
+              placeholder="Enter your number"
+            />
+
+            <legend className="my-2 text-sm font-medium">Accused name *</legend>
+            <input
+              type="text"
+              className="input rounded-sm border border-black w-full"
+              placeholder="Enter accused name"
+            />
+            <legend className="my-2 text-sm font-medium">
+              Accused phone number *
+            </legend>
+            <input
+              type="number"
+              className="input rounded-sm border border-black w-full"
+              placeholder="Enter accused number"
+            />
+            <legend className="my-2 text-sm font-medium">
+              Upload supporting evidence *
+            </legend>
+            <input
+              type="file"
+              className="file-input rounded-sm border border-black w-full"
+            />
+
+            <legend className="my-2 text-sm font-medium">
+              Summary of experience
+            </legend>
+            <textarea
+              className="textarea h-24 rounded-sm border border-black w-full"
+              placeholder="Enter your experience"></textarea>
+          </form>
+
+          <button
+            type="submit"
+            className="w-full p-4 text-xl text-black font-semibold bg-[#fdbe2e] rounded-md transition-all duration-300 ease-in-out hover:text-white hover:shadow-lg">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
